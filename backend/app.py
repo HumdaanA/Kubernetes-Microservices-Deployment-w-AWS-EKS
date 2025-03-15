0151704f6c9b40cbaa912080dd20c18efe83ff8e
HumdaanA <ahumdaan@yahoo.com>
2025-03-14 22:28:54 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -9,3 +9,17 @@ def get_data():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+'''
+After creating app.py and auth.py, run these bash commands to containerize and push to AWS ECR:
+
+
+aws ecr create-repository --repository-name auth-service
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin <AWS_ACCOUNT_ID>.dkr.ecr.us-east-1.amazonaws.com
+
+# Build and push Docker image
+docker build -t auth-service .
+docker tag auth-service:latest <AWS_ACCOUNT_ID>.dkr.ecr.us-east-1.amazonaws.com/auth-service
+docker push <AWS_ACCOUNT_ID>.dkr.ecr.us-east-1.amazonaws.com/auth-service
+
+'''
