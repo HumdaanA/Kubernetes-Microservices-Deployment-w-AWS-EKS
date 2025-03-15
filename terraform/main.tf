4dc0689349012c0ca93428ef2b20c5b7556cdc07
HumdaanA <ahumdaan@yahoo.com>
2025-03-14 22:07:31 -0500

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+
+// create a vpc with 2 public subnets
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.${count.index}.0/24"
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+}
+
+data "aws_availability_zones" "available" {}
